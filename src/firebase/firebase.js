f79a617223cf9d679beeb9fb800f54bc158220af
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-03-05 16:00:43 -0600

    Protected Firebase API

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCsCmtvFeJ-KLdomUOkSU8FCw5daIrEZ_I",
-  authDomain: "plant-pals-webpage.firebaseapp.com",
-  projectId: "plant-pals-webpage",
-  storageBucket: "plant-pals-webpage.firebasestorage.app",
-  messagingSenderId: "151908866050",
-  appId: "1:151908866050:web:79bd4a5303bc286590ef1b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-export { app, auth }
+export { app, auth };
