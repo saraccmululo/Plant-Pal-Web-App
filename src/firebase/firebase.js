c561b1e6c7032b860769e5fbb2398c33cd2e8a70
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-03-05 15:54:29 -0600

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-const firebaseConfig = {
-  apiKey: API_Key,
-  authDomain: "plant-pals-webpage.firebaseapp.com",
-  projectId: "plant-pals-webpage",
-  storageBucket: "plant-pals-webpage.firebasestorage.app",
-  messagingSenderId: "151908866050",
-  appId: "1:151908866050:web:79bd4a5303bc286590ef1b"
-};
 
 
 const app = initializeApp(firebaseConfig);
