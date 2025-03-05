74368c7bd19f404a58698b23fbfa8476272dc0d1
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-03-05 15:54:56 -0600

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,9 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-const API_KEY = import.meta.env.VITE_API_KEY;
-
-
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 
