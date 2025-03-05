b12f3768386ace6f7b4fe6f90aa19d971cf52402
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-03-05 15:36:24 -0600

    added authentication and login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { doSignInWithEmailAndPassword } from "../firebase/auth.js";
+import { useNavigate } from "react-router-dom";
+import CreateAccount from "./CreateAccount.jsx";
+
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+	const [isCreateAccount, setIsCreateAccount] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await doSignInWithEmailAndPassword(email, password);
+      navigate("/PlantDashboard"); // Change this to where you want to redirect after login
+    } catch (err) {
+      setError("Failed to log in. Please check your credentials.");
+    }
+  };
+
+	const handleClose = () =>{
+		navigate("/");
+	};
+
+  return (
+    <section className="login-container">
+      <section className="login-box">
+				<section className="close-button-container">
+					<button className="close-button" onClick={handleClose}>X</button>
+				</section>
+        <h2>{isCreateAccount? "Create an Account": "Login to add the plant"}</h2>
+        {error && <p className="error-message">{error}</p>}
+				{!isCreateAccount? (
+				<>
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+				<p>Don't have an account?</p>
+					<button className="login-account-button" onClick={() => setIsCreateAccount(true)}>Create one here</button>
+				
+				</>
+				) : (
+					<CreateAccount setIsCreateAccount={setIsCreateAccount}/>
+				)}
+			</section>
+    </section>
+  );
+};
+
+export default Login;
