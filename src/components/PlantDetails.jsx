988020f2d4022c360af31d6ee3e8e38c5ce313c7
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-03-12 15:17:32 -0500

    applied loading spinner on detail button

--- src/components/PlantDetails.jsx
+++ src/components/PlantDetails.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 
-const PlantDetail = ({ id }) => {
+const PlantDetails = ({ id }) => {
  const[plantDetails, setPlantDetails] = useState(null);
+ const [isLoading, setIsLoading] = useState(false);
+
  const API_KEY = import.meta.env.VITE_API_KEY; 
 	
  useEffect(() => {
 	async function fetchApi() {
+    setIsLoading(true);
 		try {
 			const response = await fetch(`https://perenual.com/api/v2/species/details/${id}?key=${API_KEY}`);
 			if (!response.ok) {
@@ -16,34 +19,37 @@ const PlantDetail = ({ id }) => {
 	
 		const plantDetailData = {
 			id: plantDetail.id,
-			watering: plantDetail.watering,
-			sunlight: plantDetail.sunlight.join(', '),
-			poisonous_to_pets: plantDetail.poisonous_to_pets? "Yes" : "No",
-			description: plantDetail.description,
+			watering: plantDetail.watering + ".",
+			sunlight: plantDetail.sunlight.join(', ') + ".",
+			poisonous_to_pets: (plantDetail.poisonous_to_pets? " Yes" : " No") + ".",
+			description: (` ${plantDetail.description}`),
 		};		
 		setPlantDetails(plantDetailData)
 		} catch (error) {
 			console.error(error)
-		} 
+		} finally {
+      setIsLoading(false);
+    }
 	}
 	fetchApi();
 	}, [id]);
-			
-	if (!plantDetails) {
-		return <p>Loading details...</p>;
-	}
-		
+	
+    if (isLoading) {
+      return <p className="loadingSpinner"></p>
+    }
+
+    if (!plantDetails) {
+      return <p>No details were found for this plant</p>;
+    }
+
   return (
     <section className="plant-details">
-    	{/*<img src={plantDetails.thumbnail} alt={plantDetails.common_name}/>
-    	<h3>{plantDetails.common_name.charAt(0).toUpperCase() + plantDetails.common_name.slice(1).toLowerCase()}</h3>
-    	<p><strong>Scientific name: </strong>{plantDetails.scientific_name}</p>*/}
-			<p><strong>Watering: </strong>{plantDetails.watering}</p>
+		  <p><strong>Watering: </strong>{plantDetails.watering}</p>
     	<p><strong>Light: </strong>{plantDetails.sunlight}</p>
     	<p><strong>Pet-friendly:</strong>{plantDetails.poisonous_to_pets}</p> 
-			<p><strong>Description:</strong>{plantDetails.description}</p>  
+			<p><strong>Description:</strong>{plantDetails.description}</p> 
   	</section>
-)
-}
+    
+)}
 
-export default PlantDetail;									
+export default PlantDetails;									
