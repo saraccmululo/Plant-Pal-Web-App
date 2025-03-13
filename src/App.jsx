3e1b661f9a73858c7f714992b775139b40bcc8b4
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-03-12 20:52:24 -0500

    added hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React, { useState, useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-//import logo from './assets/logo-without-background.png';
 import SearchBar from './components/SearchBar.jsx';
 import PlantList from './components/PlantList.jsx';
 import Footer from './components/Footer.jsx';
 import LoginPage from './components/LoginPage.jsx';
 import PlantDashboard from './components/PlantDashBoard.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
-//import { Link } from 'react-router-dom';
 import { AuthProvider } from './components/AuthContext';
 import { ToastContainer } from "react-toastify";
-//import LoginLogoutButton from './components/LoginLogoutButton.jsx';
 import Header from './components/Header.jsx';
 
 const App = () => {
