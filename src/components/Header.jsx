c0804e51a2c8d60c798b7ea008617577c2e39e02
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-03-13 13:52:19 -0500

    Added CSS module for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import logo from '../assets/logo-without-background.png';
 import { Link, useLocation } from 'react-router-dom';
 import LoginLogoutButton from './LoginLogoutButton.jsx';
+import styles from './Header.module.css';
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -13,29 +14,29 @@ const Header = () => {
   
   return (
     <header>
-      <figure className="logo-title"> 
-        <img src={logo} alt="Plant Pals Logo" className="logo" />
+      <figure className={styles.logoTitle}> 
+        <img src={logo} alt="Plant Pals Logo" className={styles.logo} />
           <figcaption>
           <Link to="/">
               <h1>My Plant Pals</h1> 
           </Link>
           </figcaption>
       </figure>
-      <section className="desktop-menu">
+      <section className={styles.desktopMenu}>
         {location.pathname !== '/plant-dashboard' && (
-          <Link to="/plant-dashboard" className="dashboard-link">
+          <Link to="/plant-dashboard" className={styles.dashboardLink}>
           <h4>My Plant Collection</h4>
           </Link>)}
         
         <LoginLogoutButton />
       </section>
 
-      <button className="hamburger-menu" onClick={toggleMenu}>
+      <button className={styles.hamburgerMenu} onClick={toggleMenu}>
         {showMenu? '✖' : '☰'}
       </button>
 
-      <nav className={`mobile-menu ${showMenu? 'open' : ''}`}>
-        {location.pathname !== '/plant-dashboard' && (<Link to="/plant-dashboard" className="dashboard-link">
+      <nav className={showMenu? styles.mobileMenuOpen : styles.mobileMenu}>
+        {location.pathname !== '/plant-dashboard' && (<Link to="/plant-dashboard" className={styles.dashboardLink}>
         <h4>My Plant Collection</h4>
         </Link>)}
         
