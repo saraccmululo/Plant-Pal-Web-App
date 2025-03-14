06f55740e82a9976676acd0470c1a0d7f735ecc9
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-03-13 23:03:49 -0500

    fixed resetpassword button style

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { doPasswordReset } from "../firebase/auth.js";
-import styles from './ResetPassword';
+import styles from './ResetPassword.module.css';
 
 const ResetPassword = ({ setIsResetPassword }) => {
 	const [email, setEmail] = useState("");
