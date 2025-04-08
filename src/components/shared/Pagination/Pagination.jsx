a8cdce8221ec123c8045156d640b2c7b8d1459e9
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-04-08 14:05:55 -0500

    Simpllified pagination function and increased header letter size

--- src/components/shared/Pagination/Pagination.jsx
+++ src/components/shared/Pagination/Pagination.jsx
@@ -5,25 +5,31 @@ import styles from './Pagination.module.css';
 const Pagination = ({ filteredPlants, searchTerm, filterType, filterText, onDelete, isLoading, isDashboard }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10; 
-  console.log(filteredPlants)
+  
   useEffect(() => {
-    setCurrentPage(1); // Reset to first page when search/filter changes
+    setCurrentPage(1); 
     }, [searchTerm, filterType, filterText]);
 
-  // Calculate the plants to be displayed on the current page
   const indexOfLastPlant = currentPage * itemsPerPage;
   const indexOfFirstPlant = indexOfLastPlant - itemsPerPage;
   const currentPlants = filteredPlants.slice(indexOfFirstPlant, indexOfLastPlant);
 
-  // Calculate total number of pages
   const totalPages = Math.ceil(filteredPlants.length / itemsPerPage);
-  console.log(totalPages)
 
-  // Handle page changes
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
- 
+  
+ const pageButtons=[];
+ for (let i=0; i<totalPages; i++) {
+  pageButtons.push(
+    <button key={i + 1} 
+    onClick={() => handlePageChange(i + 1)}
+    className={styles.pageButton}
+    > {i + 1}
+    </button>)
+ }
+
   return (
     <>
     <section>
@@ -36,7 +42,7 @@ const Pagination = ({ filteredPlants, searchTerm, filterType, filterText, onDele
         isDashboard={isDashboard}
       />
     </section>
-    {/* Pagination controls */}
+    {/* Pagination buttons */}
       {filteredPlants.length > 0 && (
       <section className={styles.pageButtonContainer}>
         <button 
@@ -47,15 +53,7 @@ const Pagination = ({ filteredPlants, searchTerm, filterType, filterText, onDele
           Previous
         </button>
         
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button 
-            key={index + 1} 
-            onClick={() => handlePageChange(index + 1)}
-            className={styles.pageButton}
-          >
-            {index + 1}
-          </button>
-        ))}
+        {pageButtons}
         
         <button 
           onClick={() => handlePageChange(currentPage + 1)} 
